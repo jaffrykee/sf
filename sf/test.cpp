e07975114bb0d050a54aa83b3e3f41d90283e721
liumk <jaffrykee@hotmail.com>
2014-11-14 18:33:43 +0800

    日常

---
+++ sf/test.cpp
@@ -0,0 +1,13 @@
+﻿#pragma execution_character_set("utf-8")
+#include <stdio.h>
+#include <msxml.h>
+#include <comutil.h>
+#pragma comment(lib, "comsuppwd.lib") 
+
+void main()
+{
+	IXMLDOMDocument.get_url("./data/s.xml");
+
+	printf("a");
+	getchar();
+}
