e82774edd870d22abe993ae03a1635378346baa1
liumk <jaffrykee@hotmail.com>
2015-03-13 19:43:03 +0800

    日常

--- UIEditor/BoloRes/DrawImg.xaml.cs
+++ UIEditor/BoloRes/DrawImg.xaml.cs
@@ -49,53 +49,67 @@ namespace UIEditor.BoloUI
 		{
 			if (m_loaded == false)
 			{
+				this.mx_imgFrame.ToolTip = m_path;
+
 				MainWindow pW = Window.GetWindow(this) as MainWindow;
 				double iH, iW;
 
 				m_imgHeight = double.Parse(mx_imgHeight.ToolTip.ToString())*3/4;
 				m_imgWidth = double.Parse(mx_imgWidth.ToolTip.ToString())*3/4;
-				double perX = 0.0d, perY = 0.0d, perDx = 0.0d, perDy = 0.0d;
+				double perX = 1.0d, perY = 1.0d, perDx = 0.0d, perDy = 0.0d;
 
 				if (m_xe.GetAttribute("Height") != null && m_xe.GetAttribute("Height") != "")
 				{
 					iH = double.Parse(m_xe.GetAttribute("Height"));
+
+					this.mx_ctrR0.Height = new GridLength(iH);
+					this.mx_ctrR0.MinHeight = iH;
 				}
 				else
 				{
 					iH = m_imgHeight;
+					this.mx_ctrR0.Height = new GridLength(iH);
+					this.mx_ctrR0.MinHeight = iH;
 				}
 				if (m_xe.GetAttribute("Width") != null && m_xe.GetAttribute("Width") != "")
 				{
 					iW = double.Parse(m_xe.GetAttribute("Width"));
+					this.mx_ctrC0.Width = new GridLength(iW);
+					this.mx_ctrC0.MinWidth = iW;
 				}
 				else
 				{
 					iW = m_imgWidth;
+					this.mx_ctrC0.Width = new GridLength(iW);
+					this.mx_ctrC0.MinWidth = iW;
 				}
-				this.mx_imgFrame.Width = iW;
-				this.mx_imgFrame.Height = iH;
 				if (m_xe.GetAttribute("NineGrid") != null)
 				{
-
 					if (m_xe.GetAttribute("NineGrid") == "true")
 					{
 						if (m_xe.GetAttribute("NGX") != null)
 						{
 							this.mx_ngc0.Width = new GridLength(double.Parse(m_xe.GetAttribute("NGX")));
-							this.mx_ngr2.Height = new GridLength((double)m_imgWidth - double.Parse(m_xe.GetAttribute("NGX")));
+							this.mx_ngc2.Width = new GridLength((double)m_imgWidth - double.Parse(m_xe.GetAttribute("NGX")));
 							perX = double.Parse(m_xe.GetAttribute("NGX")) / (double)m_imgWidth;
+							this.mx_gsChangeHeight.Visibility = System.Windows.Visibility.Visible;
 						}
 						else
 						{
+							this.mx_ngc0.Width = new GridLength((double)m_imgWidth);
+							this.mx_gsChangeHeight.Visibility = System.Windows.Visibility.Collapsed;
 						}
 						if (m_xe.GetAttribute("NGY") != null)
 						{
 							this.mx_ngr0.Height = new GridLength(double.Parse(m_xe.GetAttribute("NGY")));
 							this.mx_ngr2.Height = new GridLength((double)m_imgHeight - double.Parse(m_xe.GetAttribute("NGY")));
 							perY = double.Parse(m_xe.GetAttribute("NGY")) / (double)m_imgHeight;
+							this.mx_gsChangeWidth.Visibility = System.Windows.Visibility.Visible;
 						}
 						else
 						{
+							this.mx_ngr0.Height = new GridLength((double)m_imgHeight);
+							this.mx_gsChangeWidth.Visibility = System.Windows.Visibility.Collapsed;
 						}
 						if (m_xe.GetAttribute("NGWidth") != null)
 						{
@@ -111,35 +125,43 @@ namespace UIEditor.BoloUI
 						else
 						{
 						}
-
-						Rect[][] viewRect = new Rect[3][];
-						viewRect[0] = new Rect[3];
-						viewRect[1] = new Rect[3];
-						viewRect[2] = new Rect[3];
-
-						viewRect[0][0] = new Rect(0, 0, perX, perY);
-						viewRect[0][1] = new Rect(perX, 0, perDx, perY);
-						viewRect[0][2] = new Rect(perX, 0, 1 - perX, perY);
-						viewRect[1][0] = new Rect(0, perY, perX, perDy);
-						viewRect[1][1] = new Rect(perX, perY, perDx, perDy);
-						viewRect[1][2] = new Rect(perX, perY, 1 - perX, perDy);
-						viewRect[2][0] = new Rect(0, perY, perX, 1 - perY);
-						viewRect[2][1] = new Rect(perX, perY, perDx, 1 - perY);
-						viewRect[2][2] = new Rect(perX, perY, 1 - perX, 1 - perY);
-
-						this.mx_ngBrush00.Viewbox = viewRect[0][0];
-						this.mx_ngBrush01.Viewbox = viewRect[0][1];
-						this.mx_ngBrush02.Viewbox = viewRect[0][2];
-						this.mx_ngBrush10.Viewbox = viewRect[1][0];
-						this.mx_ngBrush11.Viewbox = viewRect[1][1];
-						this.mx_ngBrush12.Viewbox = viewRect[1][2];
-						this.mx_ngBrush20.Viewbox = viewRect[2][0];
-						this.mx_ngBrush21.Viewbox = viewRect[2][1];
-						this.mx_ngBrush22.Viewbox = viewRect[2][2];
+					}
+					else
+					{
+						this.mx_gsChangeHeight.Visibility = System.Windows.Visibility.Collapsed;
+						this.mx_gsChangeWidth.Visibility = System.Windows.Visibility.Collapsed;
 					}
 				}
+				else
+				{
+					this.mx_gsChangeHeight.Visibility = System.Windows.Visibility.Collapsed;
+					this.mx_gsChangeWidth.Visibility = System.Windows.Visibility.Collapsed;
+				}
 
-				this.mx_imgFrame.ToolTip = m_path;
+				Rect[][] viewRect = new Rect[3][];
+				viewRect[0] = new Rect[3];
+				viewRect[1] = new Rect[3];
+				viewRect[2] = new Rect[3];
+
+				viewRect[0][0] = new Rect(0, 0, perX, perY);
+				viewRect[0][1] = new Rect(perX, 0, perDx, perY);
+				viewRect[0][2] = new Rect(perX, 0, 1 - perX, perY);
+				viewRect[1][0] = new Rect(0, perY, perX, perDy);
+				viewRect[1][1] = new Rect(perX, perY, perDx, perDy);
+				viewRect[1][2] = new Rect(perX, perY, 1 - perX, perDy);
+				viewRect[2][0] = new Rect(0, perY, perX, 1 - perY);
+				viewRect[2][1] = new Rect(perX, perY, perDx, 1 - perY);
+				viewRect[2][2] = new Rect(perX, perY, 1 - perX, 1 - perY);
+
+				this.mx_ngBrush00.Viewbox = viewRect[0][0];
+				this.mx_ngBrush01.Viewbox = viewRect[0][1];
+				this.mx_ngBrush02.Viewbox = viewRect[0][2];
+				this.mx_ngBrush10.Viewbox = viewRect[1][0];
+				this.mx_ngBrush11.Viewbox = viewRect[1][1];
+				this.mx_ngBrush12.Viewbox = viewRect[1][2];
+				this.mx_ngBrush20.Viewbox = viewRect[2][0];
+				this.mx_ngBrush21.Viewbox = viewRect[2][1];
+				this.mx_ngBrush22.Viewbox = viewRect[2][2];
 
 				m_loaded = true;
 			}
