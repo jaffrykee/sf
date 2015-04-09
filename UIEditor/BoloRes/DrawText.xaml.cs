81d75c52e94b0a278e9d6c6bf3a2e35ca449716f
liumk <jaffrykee@hotmail.com>
2015-04-09 22:18:45 +0800

    文字

--- UIEditor/BoloRes/DrawText.xaml.cs
+++ UIEditor/BoloRes/DrawText.xaml.cs
@@ -30,6 +30,7 @@ namespace UIEditor.BoloUI
 		public DrawText(XmlElement xe, string rootPath, XmlControl rootControl, string text)
 		{
 			InitializeComponent();
+			m_xe = xe;
 			m_text = text;
 			m_loaded = false;
 			m_rootControl = rootControl;
@@ -53,6 +54,9 @@ namespace UIEditor.BoloUI
 							case "X":
 							case "Y":
 							case "Anchor":
+							case "fontSize":
+							case "Color":
+							case "Color0":
 								break;
 							default:
 								{
@@ -61,21 +65,47 @@ namespace UIEditor.BoloUI
 								break;
 						}
 					}
+					this.Width = ((Canvas)this.Parent).Width;
+					this.Height = ((Canvas)this.Parent).Height;
 
 					MainWindow pW = Window.GetWindow(this) as MainWindow;
 					double iH = 0.0d, iW = 0.0d;
 					double iX = 0.0d, iY = 0.0d;
+					double fontSize = 0;
+
+					if (m_xe.GetAttribute("fontSize") != "")
+					{
+						fontSize = double.Parse(m_xe.GetAttribute("fontSize"));
+						this.mx_text.FontSize = fontSize;
+					}
+					if (m_xe.GetAttribute("Color0") != "")
+					{
+						this.mx_text.Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#" + m_xe.GetAttribute("Color0")));
+					}
+
 
 					if (m_xe.GetAttribute("Height") != null && m_xe.GetAttribute("Height") != "")
 					{
 						iH = double.Parse(m_xe.GetAttribute("Height"));
-						this.Height = iH;
+						this.mx_text.Height = iH;
+					}
+					else
+					{
+						iH = this.mx_text.Height;
 					}
 					if (m_xe.GetAttribute("Width") != null && m_xe.GetAttribute("Width") != "")
 					{
 						iW = double.Parse(m_xe.GetAttribute("Width"));
-						this.Width = iW;
+						this.mx_text.Width = iW;
 					}
+					else
+					{
+						iW = this.mx_text.Width;
+					}
+
+
+					m_rootControl.textContent.Text += "iH:" + iH + "\tiW:" + iW + "\r\n";
+
 					if (m_xe.GetAttribute("X") != "")
 					{
 						iX = double.Parse(m_xe.GetAttribute("X"));
@@ -92,6 +122,48 @@ namespace UIEditor.BoloUI
 					{
 						iY = 0;
 					}
+
+					if (m_xe.GetAttribute("Anchor") != "")
+					{
+						int anchor = int.Parse(m_xe.GetAttribute("Anchor"));
+
+						if ((anchor & 0x01) > 0)
+						{
+							//水平居中
+							//iX = iX + (((Canvas)this.Parent).Width - curCanvasWidth) / 2;
+							this.mx_text.HorizontalAlignment = System.Windows.HorizontalAlignment.Center;
+						}
+						if ((anchor & 0x02) > 0)
+						{
+							//垂直居中
+							//iY = iY + (((Canvas)this.Parent).Height - curCanvasHeight) / 2;
+							this.mx_text.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+						}
+						if ((anchor & 0x04) > 0)
+						{
+							//左对齐
+							this.mx_text.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+						}
+						if ((anchor & 0x08) > 0)
+						{
+							//右对齐
+							//iX = iX + (((Canvas)this.Parent).Width - curCanvasWidth);
+							this.mx_text.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+						}
+						if ((anchor & 0x10) > 0)
+						{
+							//上对齐
+							this.mx_text.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+						}
+						if ((anchor & 0x20) > 0)
+						{
+							//底部对齐
+							//iY = iY + (((Canvas)this.Parent).Height - curCanvasHeight);
+							this.mx_text.VerticalAlignment = System.Windows.VerticalAlignment.Bottom;
+						}
+					}
+					Canvas.SetLeft(this, iX);
+					Canvas.SetTop(this, iY);
 				}
 			}
 		}
