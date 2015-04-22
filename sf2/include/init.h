9a82f729c07082a680fecf9e92659f672288a4a4
liumk <jaffrykee@hotmail.com>
2015-04-22 17:06:40 +0800

    回归？

---
+++ sf2/include/init.h
@@ -0,0 +1,82 @@
+﻿#pragma once
+#pragma execution_character_set("utf-8")
+
+#pragma region win系统define和include
+#ifndef WINVER              // Allow use of features specific to Windows 7 or later.
+#define WINVER 0x0700       // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#ifndef _WIN32_WINNT        // Allow use of features specific to Windows 7 or later.
+#define _WIN32_WINNT 0x0700 // Change this to the appropriate value to target other versions of Windows.
+#endif
+
+#define WIN32_LEAN_AND_MEAN     // Exclude rarely-used stuff from Windows headers
+
+// C RunTime Header Files
+
+#pragma endregion
+
+using namespace std;
+
+//前置include
+
+#include <windows.h>
+
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <wchar.h>
+#include <tchar.h>
+#include <math.h>
+
+#include <d2d1.h>
+#include <d2d1helper.h>
+#include <dwrite.h>
+#include <wincodec.h>
+
+#include <string>
+#include <map>
+#include <list>
+#include <vector>
+
+#include <stdio.h>
+#include <XmlLite.h>
+#include <shlwapi.h>
+#include <comutil.h>
+#include <atlcomcli.h>
+#include <iostream>
+#include <sstream>
+#include <Fcntl.h>
+#include <io.h>
+
+#include <tkmT.h>
+
+#pragma region inline和宏等
+template<class Interface>
+inline void
+SafeRelease(
+Interface **ppInterfaceToRelease
+)
+{
+	if (*ppInterfaceToRelease != NULL)
+	{
+		(*ppInterfaceToRelease)->Release();
+
+		(*ppInterfaceToRelease) = NULL;
+	}
+}
+
+#ifndef Assert
+#if defined( DEBUG ) || defined( _DEBUG )
+#define Assert(b) do {if(!(b)) {OutputDebugStringA("Assert: " #b "\n");}} while(0)
+#else
+#define Assert(b)
+#endif //DEBUG || _DEBUG
+#endif
+
+
+#ifndef HINST_THISCOMPONENT
+EXTERN_C IMAGE_DOS_HEADER __ImageBase;
+#define HINST_THISCOMPONENT ((HINSTANCE)&__ImageBase)
+#endif
+#pragma endregion
